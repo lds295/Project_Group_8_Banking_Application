bf9fcd9a6d0774256833f6730b283aa4f32ff02e
Luke Shahan <lds295@nau.edu>
2025-11-13 20:12:57 -0700

    Revert "Update Login.jsx"
    This reverts commit e137fd166b82a8689be7792a00bacf6e8bd495ac.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import './loginsignup.css';
 
 export default function Login() {
   const navigate = useNavigate();
@@ -109,4 +108,3 @@ const styles = {
     fontWeight: "bold"
   }
 };
-
